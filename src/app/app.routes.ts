b90e32054fcd236ee755f5b32e711e4bd3ad8794
petertechy <petertechy01@gmail.com>
2025-04-11 14:47:13 +0100

    Updated app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoAddComponent } from './components/todo-add/todo-add.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TodoListComponent },
+  { path: 'add', component: TodoAddComponent },
+];
